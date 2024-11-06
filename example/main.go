41abb794f17b30e9a4c8f77b6c997fe161936587
Gustavo Henrique Pinto <gustavo.pinto@sysmap.com.br>
2024-11-06 19:17:23 +0000

    feat: add update on put

--- example/main.go
+++ example/main.go
@@ -8,30 +8,51 @@ import (
 )
 
 func main() {
-	store, err := gokvstore.NewLocalStorageStore(".")
+	store, err := gokvstore.NewStore(".", "store.db")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer store.Close()
 
-	partition, err := store.Partition("part-1")
+	partition, err := store.Partition("partition-1")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer partition.Close()
 
-	if err := partition.PUT("hello", "world"); err != nil {
+	if err := partition.Put([]byte("hello"), []byte("world!")); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := partition.Put([]byte("foo"), []byte("foobar, man this example text is old")); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := partition.Put([]byte("hello"), []byte("world! now updated!")); err != nil {
 		log.Fatal(err)
 	}
 
 	time.Sleep(5 * time.Second)
 
-	v, err := partition.GET("hello")
+	helloValue, err := partition.Get([]byte("hello"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Println(v)
+	log.Println("Found hello:", string(helloValue))
+
+	if err := partition.Delete([]byte("hello")); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Deleted hello")
+
+	barValue, err := partition.Get([]byte("foo"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Found foo:", string(barValue))
 
 	time.Sleep(5 * time.Second)
 }
